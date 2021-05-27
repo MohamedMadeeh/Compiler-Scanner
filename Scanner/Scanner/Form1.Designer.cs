6b600b79b9bacfaa7ac4a4c792fc58bde9ecb7a4
Mostafa20180606 <mostafa_20180606@fci.helwan.edu.eg>
2021-05-27 23:08:14 +0200

    Inital Project

---
+++ Scanner/Scanner/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.autocompleteMenu1 = new AutocompleteMenuNS.AutocompleteMenu();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.autocompleteMenu1.SetAutocompleteMenu(this.richTextBox1, this.autocompleteMenu1);
+            this.richTextBox1.Location = new System.Drawing.Point(4, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1094, 593);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(409, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Compile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(359, 694);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(409, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.autocompleteMenu1.SetAutocompleteMenu(this.textBox1, null);
+            this.textBox1.Location = new System.Drawing.Point(44, 681);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 77);
+            this.textBox1.TabIndex = 3;
+            // 
+            // autocompleteMenu1
+            // 
+            this.autocompleteMenu1.AllowsTabKey = true;
+            this.autocompleteMenu1.Colors = ((AutocompleteMenuNS.Colors)(resources.GetObject("autocompleteMenu1.Colors")));
+            this.autocompleteMenu1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.autocompleteMenu1.ImageList = null;
+            this.autocompleteMenu1.Items = new string[] {
+        "Yesif-Otherwise",
+        "Omw",
+        "SIMww",
+        "Chji",
+        "SeriestI",
+        "IMwf",
+        "SIMwf",
+        "NOReturn ",
+        "Reiterate",
+        "RepeatWhen",
+        "GetBack",
+        "OutLoop",
+        "Loli ",
+        "Include",
+        "Start",
+        "Last"};
+            this.autocompleteMenu1.TargetControlWrapper = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 770);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private AutocompleteMenuNS.AutocompleteMenu autocompleteMenu1;
+    }
+}
+
---
+++ Scanner/Scanner/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string code;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Open a file";
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Clear();
+                using (StreamReader sr = new StreamReader(openFile.FileName))
+                {
+                    code = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            var lexer = new Lexer(code);
+            while (true)
+            {
+                var token = lexer.NextToken();
+                if (token.Kind == SyntaxKind.EndOfFileToken)
+                    break;
+                textBox1.Text += $"{token.Kind}: '{token.Text}'";
+                if (token.Value != null)
+                    textBox1.Text += $"{token.Value}";
+                textBox1.Text += Environment.NewLine;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            code = richTextBox1.Text;
+            var lexer = new Lexer(code);
+            while (true)
+            {
+                var token = lexer.NextToken();
+                if (token.Kind == SyntaxKind.EndOfFileToken)
+                    break;
+                textBox1.Text += $"{token.Kind}: '{token.Text}'";
+                if (token.Value != null)
+                    textBox1.Text += $"{token.Value}";
+                textBox1.Text += Environment.NewLine;
+            }
+        }
+    }
+
+    enum SyntaxKind
+    {
+        Constant,
+        WhiteSpaceToken,
+        ArithmeticOperation,
+        Braces,
+        Integer,
+        String,
+        SFloat,
+        EndOfFileToken,
+        BadToken
+    }
+
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+        public object Value { get; }
+    }
+
+    class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+        private char Current
+        {
+            get
+            {
+                if (_position >= _text.Length)
+                    return '\0';
+                return _text[_position];
+            }
+        }
+
+        private void Next()
+        {
+            _position++;
+        }
+        public SyntaxToken NextToken()
+        {
+
+            int i = 0;
+            if(_position >= _text.Length)
+            {
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+            }
+                
+            if(isInteger(Current, i))
+            {
+                var start = _position;
+                while(isInteger(Current, i))
+                {
+                    Next();
+                    i++;
+                }
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.Integer, start, text, null);
+            }
+            
+            if(isDigit(Current))
+            {
+                var start = _position;
+                while (isDigit(Current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                //int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.Constant, start, text, null);
+            }
+
+            
+
+            if(isWhiteSpace(Current))
+            {
+                var start = _position;
+                while (isWhiteSpace(Current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.WhiteSpaceToken, start, text, null);
+            }
+            else if (isArithmeticOperation(Current))
+                return new SyntaxToken(SyntaxKind.ArithmeticOperation, _position++, _text.Substring(_position - 1, 1), null);
+            
+            else if (isBraces(Current))
+                return new SyntaxToken(SyntaxKind.Braces, _position++, _text.Substring(_position - 1, 1), null);
+            if (isString(Current, i))
+            {
+                var start = _position;
+                while (isString(Current, i))
+                {
+                    Next();
+                    i++;
+                }
+                
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                if (text == "Seriestl")
+                    return new SyntaxToken(SyntaxKind.String, start, text, null);
+                else
+                {
+                    _position = start;
+                    i = 0;
+                }
+                
+            }
+            if (isSSFloat(Current, i))
+            {
+                
+                var start = _position;
+                while (isSSFloat(Current, i))
+                {
+                    Next();
+                    i++;
+                }
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                if(text == "SIMwf")
+                    return new SyntaxToken(SyntaxKind.SFloat, start, text, null);
+                else
+                {
+                    _position = start;
+                    i = 0;
+                }
+            }
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+
+        
+        private static bool isDigit(char text)
+        {
+            Regex regex = new Regex(@"^\d$");
+            string test = Char.ToString(text);
+            if (regex.IsMatch(test)) return true;
+            return false;
+        }
+        private static bool isWhiteSpace(char text)
+        {
+            if (text == ' ') return true;
+            return false;
+        }
+        private static bool isArithmeticOperation(char text)
+        {
+            Regex regex = new Regex(@"[+/-/*\/]");
+            string test = Char.ToString(text);
+            if (regex.IsMatch(test)) return true;
+            return false;
+        }
+        private static bool isBraces(char text)
+        {
+            //Regex regex = new Regex(@"(\{.*?\}|\[.*?\]|\(.*?\))");
+            Regex regex = new Regex(@"(\{|\}|\[|\]|\(|\))");
+            string test = Char.ToString(text);
+            if (regex.IsMatch(test)) return true;
+            return false;
+        }
+        
+
+        private static bool isInteger(char text, int location)
+        {
+            string str = "omw";
+            if (location < str.Length && text == str[location]) return true;
+            return false;
+        }
+
+
+        private static bool isString(char text, int location)
+        {
+            string str = "Seriestl";
+            if (location < str.Length && text == str[location]) return true;
+            return false;
+        }
+        private static bool isSSFloat(char text, int location)
+        {
+            string str = "SIMwf";
+            if (location < str.Length && text == str[location]) return true;
+            return false;
+        }
+    }
+}
